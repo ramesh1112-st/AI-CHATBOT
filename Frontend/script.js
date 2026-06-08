b6b5a4d85334d232366046ea82cd8336d13a1e0f
Ramesh <ramesh.dasari2003@gmail.com>
2026-06-08 14:19:45 +0530

    changing url of website

--- Frontend/script.js
+++ Frontend/script.js
@@ -22,7 +22,7 @@ async function sendMessage() {
 
     input.value = "";
 
-    const response = await fetch("http://127.0.0.1:8000/chat", {
+    const response = await fetch("https://ai-chatbot-7pq0.onrender.com/chat", {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
